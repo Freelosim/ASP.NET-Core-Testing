f603717fe82110f5ffd3fff081b1ebfc0bcdcefa
Casper van den Dijk <486843@student.fontys.nl>
2022-04-12 17:17:38 +0200

    Add project files.

---
+++ ASP.NET Core Testing/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ASP.NET_Core_Testing.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_Core_Testing.Data
+{
+  public class AppDbContext : DbContext
+  {
+    public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+    {
+
+    }
+
+    public DbSet<Category> Categories { get; set;  }
+  }
+}
